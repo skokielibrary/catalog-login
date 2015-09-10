d73152829e1da915eff522abb988b0002d38c496
brodieaustin <brodie.austin@gmail.com>
2015-09-10 16:51:28 -0500

    changes

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'), 
+  sass = require('gulp-sass') ,
+  minifycss = require('gulp-minify-css')
+
+var config = {
+    sass_path: 'src/assets/scss',
+    css_path: 'dist/assets/css',
+    js_src_path: 'src/assets/js/src',
+    js_path: 'dist/assets/js',
+    bower_dir: 'bower_components',
+    node_dir: 'node_modules'
+}
+
+gulp.task('sass', function () {
+    gulp.src(config.sass_path + '/*.scss')
+    .pipe(sass({ includePaths : [config.sass_path, config.node_dir + '/node.normalize.scss/'] }))
+    .pipe(minifycss())
+    .pipe(gulp.dest(config.css_path))
+});
+
+
+//watch sass for changes
+gulp.task('watch', function() {
+  gulp.watch('src/assets/scss/*.scss', ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['sass', 'watch']);
